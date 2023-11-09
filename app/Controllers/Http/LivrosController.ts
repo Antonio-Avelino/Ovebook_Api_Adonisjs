383870d28ead5f1205fe3f79842c8d2909107f9c
Antonio Avelino <antonio.avelino@gideiasdinamicas.com>
2023-11-09 12:12:47 +0100

    criar chaves extrangeras

--- app/Controllers/Http/LivrosController.ts
+++ app/Controllers/Http/LivrosController.ts
@@ -1,16 +1,23 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import Livro from "App/Models/Livro";
 import Perfil from "App/Models/Perfil";
+import Sumario from "App/Models/Sumario";
 import BasesController from "./BasesController";
 export default class LivrosController extends BasesController {
   public async index({ request, response }: HttpContextContract) {
     // let dataPerfil = await Livro.all();
-    let dataPerfil = await Livro.query()
-      .where("is_deleted", false)
-      .preload("perfil");
-    // .first()
+    // let dataPerfil = await Livro.query()
+    //   .where("is_deleted", false)
+    //   .preload("perfil");
 
-    return this.suceco({ data: dataPerfil, response });
+    let dataLivro = await Sumario.query()
+    // .where('is_delete',false)
+    .preload("livro")
+    // .preload('livro',(perfil)=>{
+    //   perfil.preload('perfil')
+    // })
+
+    return this.suceco({ data: dataLivro, response });
   }
 
   public async store({ request, response, auth }: HttpContextContract) {
@@ -31,6 +38,7 @@ export default class LivrosController extends BasesController {
   public async show({ request, response, params }: HttpContextContract) {
     //  let livro = await Livro.find(params.id)
     let livro = await Livro.query()
+      .where("is_deleted", false)
       .where("id", params.id)
       .preload("perfil")
       .first();
@@ -43,4 +51,18 @@ export default class LivrosController extends BasesController {
   public async update({}: HttpContextContract) {}
 
   public async destroy({}: HttpContextContract) {}
+
+  public async inserirSumario({
+    request,
+    response,
+    params,
+  }: HttpContextContract) {
+    let body = request.all();
+    let livro = await Livro.find(params.id);
+    body.id_livro= livro?.id
+
+    if (!livro)      return this.notFound({ response, mensagem: "o livro não existe" });
+    let data = await Sumario.create(body);
+     return this.write({ response, data, mensagem: null });
+  }
 }
